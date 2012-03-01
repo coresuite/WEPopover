f82eac5ae92ab0d2a5db3f7158529caa145d156b
Tomasz Krasnyk <krt@coresystems.ch>
2012-03-01 10:06:03 +0100

    - Test change

--- main.m
+++ main.m
@@ -3,7 +3,7 @@
 //  WEPopover
 //
 //  Created by Werner Altewischer on 06/11/10.
-//  Copyright 2010 Werner IT Consultancy. All rights reserved.
+//   Copyright 2010 Werner IT Consultancy. All rights reserved.
 //
 
 
